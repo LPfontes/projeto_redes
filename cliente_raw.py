991780d565b2eba00015a3c168c8a2733c8617f1
Luiz paulo de souza fontes junior <82241012+LPfontes@users.noreply.github.com>
2024-04-30 17:07:42 -0300

    alterações cliente_raw

--- cliente_raw.py
+++ cliente_raw.py
@@ -1,20 +1,32 @@
 import socket
 import funcoes_aux 
     
+
+def definir_porta_origem():
+    sock = socket.socket()
+    sock.bind(('', 0))
+    return sock.getsockname()[1]
+def definir_ip_origem():
+    hostname = socket.gethostname()
+    return socket.gethostbyname(hostname)
+
+
 print("Tipos da requisção: ")
 print("1 Data e hora atual;")
 print("2 Uma mensagem motivacional para o fim do semestre;")
 print("3 A quantidade de respostas emitidas pelo servidor até o momento;")
 print("digite 'sair' para encerrar o programa.")
-ip_servidor = '15.228.191.109'
 
+ip_servidor = '15.228.191.109'
 porta = 50000
 while True: 
     tipo = input("Digite o tipo da requisção desejada: ")
     if(tipo == 'sair'):
         print("adeus ʕ •ɷ•ʔฅ ")
         break
-    socket_raw = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)   
+    ip_origem = definir_ip_origem()
+    porta_origem = definir_porta_origem()
+    socket_raw = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP)   
     mensagem = funcoes_aux.criar_menssagem(int(tipo),funcoes_aux.sortear_identificador()) #Cria a messangem a ser passada para o servidor de acordo com a requisão
     #criar_o_cabeçalho_UDP;
     #criar_cabeçalho_IP; 
@@ -23,5 +35,4 @@ while True:
     dados, endereco_origem = socket_raw.recvfrom(2040) #Recebe a resposta do servidor
     resposta = funcoes_aux.decodificar_resposta(dados) # Decodifica a resposta de acordo com o tipo da requesição
     print(resposta)
-
     socket_raw.close()
