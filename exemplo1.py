d8302606cbd02aae7f9c2057478c87e593505493
Luiz paulo de souza fontes junior <82241012+LPfontes@users.noreply.github.com>
2024-05-01 23:46:54 -0300

    novo exemplo

--- exemplo1.py
+++ exemplo1.py
@@ -14,25 +14,22 @@ udp_checksum = funcoes_aux.dividir_numero_2_bytes(udp_checksum)
 # 2 bytes para o numero da porta da origem, 2 bytes para o numero da porta de destino, 2 bytes para o tamanho do cabeçalho e 2 bytes para o cheksum
 udp_cabecalho = bytes([porta_origem[0], porta_origem[1], porta_destino[0],porta_destino[1],tamanho_udp_cabecalho[0],tamanho_udp_cabecalho[1],udp_checksum[0],udp_checksum[1]]) 
 segmento = udp_cabecalho + data
-
 # na especificação do udp RFC 768  está definido que o cheksum do udp é feito com um pseudo_cabecalho com informações do cabecalho ip + o cabeçalho udp
 # 16 bytes para o ip_origem 
 # 16 bytes para o ip_destino 
 # 1 byte zero
 # 1 byte com o codigo do protocolo
 # 2 bytes com o tamanho do segmento udp
+
 ip_origem = '192.168.001.105'
 ip_destino = '15.228.191.109'
 protocolo = socket.IPPROTO_UDP
-x = (105).to_bytes()
-
 ip_origem = funcoes_aux.trasformar_string_ip_em_bytes(ip_origem)
 ip_destino = funcoes_aux.trasformar_string_ip_em_bytes(ip_destino)
 protocolo = protocolo.to_bytes(1,byteorder='big')
 zero = 0b00000000
 pseudo_cabecalho = ip_origem + ip_destino + bytes([zero,protocolo[0], tamanho_udp_cabecalho[0],tamanho_udp_cabecalho[1]])  
-tamanho = len(pseudo_cabecalho)
-tamanho = len(segmento)
+
 cheksum = funcoes_aux.cheksum(pseudo_cabecalho + segmento)
 print(udp_cabecalho)
 print("porta_origem")
