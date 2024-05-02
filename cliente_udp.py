11629a80dd45e5f812d74ffa98d49a63f4bad644
Bárbara Hellen <barbarahellen993@gmail.com>
2024-05-02 16:06:39 -0300

    organização do código

--- cliente_udp.py
+++ cliente_udp.py
@@ -1,23 +1,23 @@
 import socket
-import funcoes_aux 
-    
-print("Tipos da requisção: ")
-print("1 Data e hora atual;")
-print("2 Uma mensagem motivacional para o fim do semestre;")
-print("3 A quantidade de respostas emitidas pelo servidor até o momento;")
-print("digite 'sair' para encerrar o programa.")
+import funcoes_aux
 
-while True: 
-    tipo = input("Digite o tipo da requisção desejada: ")
-    if(tipo == 'sair'):
+print("Tipos de requisição: ")
+print("1 - Data e hora atual;")
+print("2 - Uma mensagem motivacional para o fim do semestre;")
+print("3 - A quantidade de respostas emitidas pelo servidor até o momento;")
+print("4 - Sair")
+
+while True:
+    tipo = input("Digite o tipo da requisição desejada: ")
+    if (tipo == "4"):
         print("adeus ʕ •ɷ•ʔฅ ")
         break
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Cria o socket tipo UDP que Ficará responsável por criar os cabeçalhos IP e UDP 
-    mensagem = funcoes_aux.criar_requisao(int(tipo),funcoes_aux.sortear_identificador()) #Cria a messangem a ser passada para o servidor de acordo com a requisão
-    endereco_destino = ('15.228.191.109', 50000) #Endereço do servidor
-    udp_socket.sendto(mensagem, endereco_destino) # Envia a messagem
-    dados, endereco_origem = udp_socket.recvfrom(2040) #Recebe a resposta do servidor
-    resposta = funcoes_aux.decodificar_resposta(dados) # Decodifica a resposta de acordo com o tipo da requesição
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Cria o socket tipo UDP que Ficará responsável por criar os cabeçalhos IP e UDP
+    mensagem = funcoes_aux.criar_requisao(int(tipo), funcoes_aux.sortear_identificador()) # Cria a mensagem a ser passada para o servidor de acordo com a requisão
+    endereco_destino = ('15.228.191.109', 50000)        # Endereço do servidor
+    udp_socket.sendto(mensagem, endereco_destino)       # Envia a messagem
+    dados, endereco_origem = udp_socket.recvfrom(2040)  # Recebe a resposta do servidor
+    resposta = funcoes_aux.decodificar_resposta(dados)  # Decodifica a resposta de acordo com o tipo da requesição
     print(resposta)
 
     udp_socket.close()
--- funcoes_aux.py
+++ funcoes_aux.py
@@ -5,78 +5,87 @@ consts.DATA = 1
 consts.MENSAGEM_MOTIVACIONAL = 2
 consts.QUANTIDADE_RESPOSTAS_SERVIDOR = 3
 
+
 def cheksum(lista_bytes):
     tamanho_lista = len(lista_bytes)
-    if tamanho_lista % 2 == 0: # Verfica se o tamanho da lista_bytes é par 
+    if tamanho_lista % 2 == 0:                 # Verifica se o tamanho da lista_bytes é par
        pass
     else:
         lista_bytes = lista_bytes + bytes([0]) # Se não, adiciona um byte zero no fim da lista
-    tamanho_lista = len(lista_bytes) # Atualiza o tamanho da lista
+    tamanho_lista = len(lista_bytes)           # Atualiza o tamanho da lista
     index = 0
     resultado = 0
     for i in range(int(tamanho_lista/4)):
         # Soma as palavras de 16 bits na lista e soma com a palavra guardada em resultado
-        resultado = somar_palavra_16bits(resultado,somar_palavra_16bits(lista_bytes[index] << 8 | lista_bytes[index+1],lista_bytes[index+2]<< 8 |lista_bytes[index+3])) 
-        index += 4 
-    return ~resultado & 0xFFFF #inverte os bits do resultado
-        
-def somar_palavra_16bits(palavra1,palavra2):
-    resultado =  palavra1 + palavra2 
-    while resultado.bit_length() > 16: # Verifica se a soma da palavra1 + palavra2 tem mais de 16 bits se sim um carryout do bit mais significativo vai ser somada ao resultado
+        resultado = somar_palavra_16bits(resultado, somar_palavra_16bits(lista_bytes[index] << 8 | lista_bytes[index + 1],lista_bytes[index + 2] << 8 | lista_bytes[index + 3]))
+        index += 4
+    return ~resultado & 0xFFFF           # inverte os bits do resultado
+
+
+def somar_palavra_16bits(palavra1, palavra2):
+    resultado = palavra1 + palavra2
+    while resultado.bit_length() > 16:   # Verifica se a soma da palavra1 + palavra2 tem mais de 16 bits, se sim um carryout do bit mais significativo vai ser somada ao resultado
         carry = resultado >> 16
         resultado &= 0xFFFF
         resultado += carry
     return resultado
 
-def dividir_numero_2_bytes(numero): # Divide um int em uma lista com 2 bytes seguindo a ordenação big endian
-    numero = numero.to_bytes(2,byteorder='big')
+
+def dividir_numero_2_bytes(numero):     # Divide um int em uma lista com 2 bytes seguindo a ordenação big endian
+    numero = numero.to_bytes(2, byteorder='big')
     return numero
 
-def trasformar_string_ip_em_bytes(ip): # Remove os pontos da string ip e trasforma a string em uma lista de bytes 
+
+def trasformar_string_ip_em_bytes(ip):  # Remove os pontos da string ip e trasforma a string em uma lista de bytes
     lista_string = ip.split('.')
     lista_int = []
     for i in lista_string:
-        lista_int.append(int(i)) 
+        lista_int.append(int(i))
     return bytes(lista_int)
-    
+
+
 def sortear_identificador():  # Retorna um numero aleatorio de 1 a 65535
     return random.randint(1, 65535)
 
-def criar_requisao(tipo,identificador): # Codifica a requisão de acordo com as especificação do projeto
+
+def criar_requisao(tipo, identificador):  # Codifica a requisão de acordo com as especificação do projeto
     byte1 = 0b00000000
-    bytes_identificador = dividir_numero_2_bytes(identificador) 
+    bytes_identificador = dividir_numero_2_bytes(identificador)
     match tipo:
         case consts.DATA:
             pass
         case consts.MENSAGEM_MOTIVACIONAL:
             byte1 = byte1 | 0b0001
-             
+
         case consts.QUANTIDADE_RESPOSTAS_SERVIDOR:
             byte1 = byte1 | 0b0010
-            
-    mensagem = bytes([byte1, bytes_identificador[0], bytes_identificador[1]]) 
-    return mensagem     
-  
-def bytes_to_string(lista_bytes): # Trasforma uma lista de bytes em um string
+
+    mensagem = bytes([byte1, bytes_identificador[0], bytes_identificador[1]])
+    return mensagem
+
+
+def bytes_to_string(lista_bytes):  # Trasforma uma lista de bytes em um string
     return ''.join(chr(i) for i in lista_bytes)
 
-def bytes_to_int(lista_bytes): # Trasforma uma lista de bytes em um int
+
+def bytes_to_int(lista_bytes):  # Trasforma uma lista de bytes em um int
     return int.from_bytes(bytes(lista_bytes), byteorder='big')
-   
-def decodificar_resposta(dados): # Decodifica a resposta de acordo com as especificação do projeto
+
+
+def decodificar_resposta(dados):  # Decodifica a resposta de acordo com as especificações do projeto
     tipo = dados[0]
     tamanho_resposta = dados[3]
     lista_bytes = []
     match tipo:
         case 0x10:
             for i in range(tamanho_resposta):
-                lista_bytes.append(dados[4+i]) 
+                lista_bytes.append(dados[4+i])
             return bytes_to_string(lista_bytes)
         case 0x11:
             for i in range(tamanho_resposta):
-                lista_bytes.append(dados[4+i]) 
+                lista_bytes.append(dados[4+i])
             return bytes_to_string(lista_bytes)
         case 0x12:
             for i in range(tamanho_resposta):
-                lista_bytes.append(dados[4+i]) 
+                lista_bytes.append(dados[4+i])
             return bytes_to_int(lista_bytes)
