5960aa10caeefe749ae374159a14897043e61dcc
Bárbara Hellen <barbarahellen993@gmail.com>
2024-05-02 16:35:45 -0300

    organização do código

--- cliente_raw.py
+++ cliente_raw.py
@@ -1,36 +1,39 @@
 import socket
-import funcoes_aux 
-    
-def definir_porta_origem(): #Solicita uma porta livre ao SO
+import funcoes_aux
+
+
+def definir_porta_origem():  # Solicita uma porta livre ao SO
     sock = socket.socket()
     sock.bind(('', 0))
     return sock.getsockname()[1]
 
-def definir_ip_origem(): # Recebe o IP do host
+
+def definir_ip_origem():     # Recebe o IP do host
     hostname = socket.gethostname()
     return socket.gethostbyname(hostname)
 
-print("Tipos da requisção: ")
-print("1 Data e hora atual;")
-print("2 Uma mensagem motivacional para o fim do semestre;")
-print("3 A quantidade de respostas emitidas pelo servidor até o momento;")
-print("digite 'sair' para encerrar o programa.")
+
+print("Tipos de requisição: ")
+print("1 - Data e hora atual")
+print("2 - Uma mensagem motivacional para o fim do semestre")
+print("3 - A quantidade de respostas emitidas pelo servidor até o momento")
+print("4 - Sair")
 
 ip_servidor = '15.228.191.109'
 porta = 50000
-while True: 
-    tipo = input("Digite o tipo da requisção desejada: ")
-    if(tipo == 'sair'):
+while True:
+    tipo = input("Digite o tipo da requisição desejada: ")
+    if (tipo == '4'):
         print("adeus ʕ •ɷ•ʔฅ ")
         break
     ip_origem = definir_ip_origem()
     porta_origem = definir_porta_origem()
-    socket_raw = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP) # Cria o socket tipo RAW que Ficará responsável apenas pelo cabeçalho UDP    
-    mensagem = funcoes_aux.criar_requisao(int(tipo),funcoes_aux.sortear_identificador()) #Cria a messangem a ser passada para o servidor de acordo com  a requisão
-    #criar_o_cabeçalho_UDP;
-    endereco_destino = (ip_servidor, porta) #Endereço do servidor
+    socket_raw = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP) # Cria o socket tipo RAW que ficará responsável apenas pelo cabeçalho UDP
+    mensagem = funcoes_aux.criar_requisao(int(tipo), funcoes_aux.sortear_identificador()) # Cria a mensagem a ser passada para o servidor de acordo com  a requisição
+    # criar_o_cabeçalho_UDP;
+    endereco_destino = (ip_servidor, porta)       # Endereço do servidor
     socket_raw.sendto(mensagem, endereco_destino)
-    dados, endereco_origem = socket_raw.recvfrom(2040) #Recebe a resposta do servidor
-    #resposta = decodificar resposta do servidor ignorado o cabeçalho ip e o udp
-    #print(resposta)
+    dados, endereco_origem = socket_raw.recvfrom(2040)  # Recebe a resposta do servidor
+    # resposta = decodificar resposta do servidor ignorado o cabeçalho ip e o udp
+    # print(resposta)
     socket_raw.close()
--- cliente_udp.py
+++ cliente_udp.py
@@ -2,9 +2,9 @@ import socket
 import funcoes_aux
 
 print("Tipos de requisição: ")
-print("1 - Data e hora atual;")
-print("2 - Uma mensagem motivacional para o fim do semestre;")
-print("3 - A quantidade de respostas emitidas pelo servidor até o momento;")
+print("1 - Data e hora atual")
+print("2 - Uma mensagem motivacional para o fim do semestre")
+print("3 - A quantidade de respostas emitidas pelo servidor até o momento")
 print("4 - Sair")
 
 while True:
@@ -12,12 +12,12 @@ while True:
     if (tipo == "4"):
         print("adeus ʕ •ɷ•ʔฅ ")
         break
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Cria o socket tipo UDP que Ficará responsável por criar os cabeçalhos IP e UDP
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Cria o socket tipo UDP que ficará responsável por criar os cabeçalhos IP e UDP
     mensagem = funcoes_aux.criar_requisao(int(tipo), funcoes_aux.sortear_identificador()) # Cria a mensagem a ser passada para o servidor de acordo com a requisão
     endereco_destino = ('15.228.191.109', 50000)        # Endereço do servidor
-    udp_socket.sendto(mensagem, endereco_destino)       # Envia a messagem
+    udp_socket.sendto(mensagem, endereco_destino)       # Envia a mensagem
     dados, endereco_origem = udp_socket.recvfrom(2040)  # Recebe a resposta do servidor
-    resposta = funcoes_aux.decodificar_resposta(dados)  # Decodifica a resposta de acordo com o tipo da requesição
+    resposta = funcoes_aux.decodificar_resposta(dados)  # Decodifica a resposta de acordo com o tipo da requisição
     print(resposta)
 
     udp_socket.close()
