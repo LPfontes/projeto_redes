ac19eb0824ab423010e4d8c9f144015318d6c6be
Luiz paulo de souza fontes junior <82241012+LPfontes@users.noreply.github.com>
2024-05-02 14:19:25 -0300

    novo exemplo

--- cliente_raw.py
+++ cliente_raw.py
@@ -27,13 +27,12 @@ while True:
         break
     ip_origem = definir_ip_origem()
     porta_origem = definir_porta_origem()
-    socket_raw = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)   
-    mensagem = funcoes_aux.criar_menssagem(int(tipo),funcoes_aux.sortear_identificador()) #Cria a messangem a ser passada para o servidor de acordo com a requisão
+    socket_raw = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP)   
+    mensagem = funcoes_aux.criar_requisao(int(tipo),funcoes_aux.sortear_identificador()) #Cria a messangem a ser passada para o servidor de acordo com a requisão
     #criar_o_cabeçalho_UDP;
-    #criar_cabeçalho_IP; 
     endereco_destino = (ip_servidor, porta) #Endereço do servidor
     socket_raw.sendto(mensagem, endereco_destino)
     dados, endereco_origem = socket_raw.recvfrom(2040) #Recebe a resposta do servidor
-    resposta = funcoes_aux.decodificar_resposta(dados) # Decodifica a resposta de acordo com o tipo da requesição
-    print(resposta)
+    #resposta = decodificar resposta do servidor ignorado o cabeçalho ip e o udp
+    #print(resposta)
     socket_raw.close()
--- exemplo1.py
+++ exemplo1.py
@@ -1,8 +1,20 @@
 import socket
 import funcoes_aux
 #exemplo dado pelo professor nas Especificaçãos
-data = bytes([0b00000010, 0b01011100, 0b11100001])
-porta_origem = 59155
+
+def definir_porta_origem():
+    sock = socket.socket()
+    sock.bind(('', 0))
+    return sock.getsockname()[1]
+
+def definir_ip_origem():
+    hostname = socket.gethostname()
+    return socket.gethostbyname(hostname)
+
+data = funcoes_aux.criar_requisao(1,funcoes_aux.sortear_identificador())
+
+porta_origem = definir_porta_origem()
+socket_raw = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP) 
 porta_destino = 50000
 tamanho_udp_cabecalho = 8 + len(data)  # Tamanho do cabeçalho UDP + dados
 udp_checksum = 0  # O checksum pode ser calculado posteriormente
@@ -21,7 +33,7 @@ segmento = udp_cabecalho + data
 # 1 byte com o codigo do protocolo
 # 2 bytes com o tamanho do segmento udp
 
-ip_origem = '192.168.001.105'
+ip_origem = definir_ip_origem()
 ip_destino = '15.228.191.109'
 protocolo = socket.IPPROTO_UDP
 ip_origem = funcoes_aux.trasformar_string_ip_em_bytes(ip_origem)
@@ -42,5 +54,9 @@ print("tamanho cabeçalho")
 print(funcoes_aux.bytes_to_int([udp_cabecalho[4],udp_cabecalho[5]]))
 print("cheksum")
 print(funcoes_aux.bytes_to_int([udp_cabecalho[6],udp_cabecalho[7]]))
+endereco_destino = ('15.228.191.109', 50000) #Endereço do servidor
+socket_raw.sendto(segmento, endereco_destino)
 
+dados, endereco_origem = socket_raw.recvfrom(2040) #Recebe a resposta do servidor
+print(dados)
 
